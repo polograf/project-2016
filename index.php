4166661ab4ec3dcb8ebe883f631c9e924ee23fa4
Paulina Woś <paulinawos1@MacBook-Air-Paulina.local>
2016-11-21 19:10:37 +0100

    add

--- index.php
+++ index.php
@@ -5,15 +5,20 @@ class Samochod
     public function __construct($pojemnoscSilnika, $rokProdukcji, $kolor = 'czerwony')
     {
         $this->rokProdukcji = $rokProdukcji;
-
     }
 
     public function __getRokProdukcji()
     {
         return $this->rokProdukcji;
     }
+    public function setRokProdukcji($rokProdukcji)
+    {
+        $this->rokProdukcji = $rokProdukcji;
+    }
 }
 
-$ferrari = new Samochod('6000','2010');
+$ferrari = new Samochod('6000',2010);
 
+
+$ferrari->setRokProdukcji(2016)
 echo $ferrari->getRokProdukcji();
